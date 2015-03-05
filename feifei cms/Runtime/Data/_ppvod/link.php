96f72d0ae8535b5971fa41ad88f99dc457630aa3
ishiyong2015 <ishiyong2015@163.com>
2015-03-05 10:24:28 +0800

    feifei

---
+++ feifei cms/Runtime/Data/_ppvod/link.php
@@ -0,0 +1,13 @@
+<?php
+return array (
+  0 => 
+  array (
+    'link_id' => '1',
+    'link_name' => '飞飞影视导航系统',
+    'link_logo' => 'http://',
+    'link_url' => 'http://www.ppvod.net',
+    'link_order' => '1',
+    'link_type' => '1',
+  ),
+);
+?>
